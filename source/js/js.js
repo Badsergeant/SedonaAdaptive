a172d5f61253dcbe48dbb8988367be2103002188
unknown <jok67rus@gmail.com>
2025-03-28 13:30:09 +0300

    Fix js code for modal-failure

--- source/js/js.js
+++ source/js/js.js
@@ -19,20 +19,37 @@ navToggle.addEventListener('click', function () {
 /* popup js*/
 
 var modalSucces = document.querySelector('.modal-succes');
+var modalFail = document.querySelector('.modal-failure');
 var findButton = document.querySelector('.item-emotions__button');
 var shutDownBtn = document.querySelector('.modal__button');
+var inputValid = document.querySelectorAll('input[required]').value;
+
 
 findButton.addEventListener('click', function () {
-  if (modalSucces.classList.contains('modal-succes--closed')) {
-    modalSucces.classList.remove('modal-succes--closed');
-    modalSucces.classList.add('modal-succes--opened');
+  if (inputValid == " ") {
+    if (modalFail.classList.contains('modal-failure--closed')) {
+      modalFail.classList.remove('modal-failure--closed');
+      modalFail.classList.add('modal-failure--opened');
+    }
+    if (modalFail.classList.contains('modal-failure--opened')) {
+      shutDownBtn.addEventListener('click', function () {
+        modalFail.classList.remove('modal-failure--opened');
+        modalFail.classList.add('modal-failure--closed');
+      })
+    return false;
+    }
+
+  } else {
+    if (modalSucces.classList.contains('modal-succes--closed')) {
+      modalSucces.classList.remove('modal-succes--closed');
+      modalSucces.classList.add('modal-succes--opened');
+    }
+    if (modalSucces.classList.contains('modal-succes--opened')) {
+      shutDownBtn.addEventListener('click', function () {
+        modalSucces.classList.remove('modal-succes--opened');
+        modalSucces.classList.add('modal-succes--closed');
+      });
+    }
   }
-  if (modalSucces.classList.contains('modal-succes--opened')) {
-    shutDownBtn.addEventListener('click', function () {
-      modalSucces.classList.remove('modal-succes--opened');
-      modalSucces.classList.add('modal-succes--closed');
-    });
-    /* modalSucces.classList.remove('modal-succes--opened');
-    modalSucces.classList.add('modal-succes--closed'); */
-  }
+
 });
